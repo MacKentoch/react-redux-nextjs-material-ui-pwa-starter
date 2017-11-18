56195b7c4a20eb48fc74c993857c3c3fd8fdfd42
MacKentoch <erwan.datin@gmail.com>
2017-11-18 07:14:43 +0100

    Layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -13,7 +13,6 @@ import IconButton         from 'material-ui/IconButton';
 import Hidden             from 'material-ui/Hidden';
 import Divider            from 'material-ui/Divider';
 import MenuIcon           from 'material-ui-icons/Menu';
-import List               from 'material-ui/List';
 import {
   Menu1,
   Menu2
